fdf9a43c3db338cbec46d9f3719785455e3463ba
yash27032002 <yashahlawat7355@gmail.com>
2024-08-19 00:14:46 +0530

    commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            "Get things done, one checkmark at a time!"
+            <p>Our to-do list app is designed to help you stay organized, focused, and productive. Whether it's big tasks or small reminders, add them to your list, check them off as you go, and watch your productivity soar. Simplify your life, one task at a time!"
+
+
+
+
+
+
+</p>
+        </div>
+    )
+}
